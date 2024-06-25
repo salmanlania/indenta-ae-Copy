b9db28111c2b9c8ed84fc556ef43026b026a0f3e
indenta-ai <shahbazabdullah81@gmail.com>
2024-06-25 16:59:05 +0500

    chatbot chatbotys

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,6 @@ import Commitment from "../components/Home2/Commitment1";
 import Testimonial from "../components/Home2/Testimonial1";
 import Process from "../components/Home2/Process1";
 import OurServices from "../components/Home2/OurServicesHome";
-import TeamHome from "../components/Home2/TeamHome";
 import EventAndConfarance from "../components/Home2/EventAndConfarance-main";
 import ProjectSection from "../components/Home2/ProjectSection";
 import WhyChooseSection from "../components/Home2/WhyChooseSection1";
@@ -57,7 +56,7 @@ export default function Home() {
       <Aboutus/>
       {/* <SolutionSection/> */}
       <OurServices/>
-      <EventAndConfarance/>
+      {/* <EventAndConfarance/> */}
       <VideoArea/>
       <WhyUs/>
       {/* <ProjectSection/> */}
